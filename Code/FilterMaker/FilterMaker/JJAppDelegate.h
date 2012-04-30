7a81f5f78fc3c1a84a58657a7cbea28ac9837714
Josh Johnson <jnjosh@gmail.com>
2012-04-30 19:24:11 -0400

    initial

---
+++ Code/FilterMaker/FilterMaker/JJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JJAppDelegate.h
+//  FilterMaker
+//
+//  Created by Joshua Johnson on 4/30/12.
+//  Copyright (c) 2012 Two Toasters, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Code/FilterMaker/FilterMaker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FilterMaker
+//
+//  Created by Joshua Johnson on 4/30/12.
+//  Copyright (c) 2012 Two Toasters, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JJAppDelegate class]));
+    }
+}
